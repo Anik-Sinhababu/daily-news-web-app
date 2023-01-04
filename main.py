8282d08b995437d03a592d008eaad11689d54e9e
ANIK <sinhababuanik@gmail.com>
2023-01-04 15:22:11 +0530

    Daily news web app

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import requests
+from send_mail import send_email
 api_key = "5628c780313a460f8a1aa361871463da"
 url = "https://newsapi.org/v2/top-headlines?" \
       "country=in&apiKey=5628c780313a460f8a1aa361871463da"
-#Make request
+# Make request
 req = requests.get(url)
-#Get a ditionary with data
+# Get a dictionary with data
 content = req.json()
-#Access ata with their titles
-print(content["articles"])
+# Access ata with their titles
+message = ""
 for article in content["articles"]:
-      print(article["title"])
+    if article["title"] and article["description"] is not None:
+        message = message + article["title"] + "\n" + \
+                  article["description"] + "\n" + article["url"] + "\n\n"
 
+
+message = message.encode("utf-8")
+send_email(message)
